eafd260ef79d8689f4cb987e8e2210c826ca2602
FREDYK1 <fkwekukankam@gmail.com>
2024-10-27 22:19:59 +0000

    Click the login button

--- main.py
+++ main.py
@@ -5,23 +5,27 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-# Define driver,options and service
+# Define driver, options, and service
 chrome_options = Options()
 chrome_options.add_argument("--disable-search-engine-choice-screen")
 service = Service("chromedriver-win64/chromedriver.exe")
-driver = webdriver.Chrome(options=chrome_options,service=service)
+driver = webdriver.Chrome(options=chrome_options, service=service)
 
-# Open the browser and navigate to the URL
-driver.get("https://demoqa.com/login")
+try:
+    # Open the browser and navigate to the URL
+    driver.get("https://demoqa.com/login")
 
-#Locate username and password fields
-username_field = WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.ID,"userName")))
-password_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "password")))
+    # Locate username, password, and login button fields
+    username_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "userName")))
+    password_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "password")))
+    login_button = driver.find_element(By.ID, "login")
 
-# Insert username and password
-username_field.send_keys("frederickkankam7@gmail.com")
-password_field.send_keys("F1R1E1d11")
+    # Insert username, password, and click login button
+    username_field.send_keys("FREDYKcode1")
+    password_field.send_keys("FREDYK@code1")
+    driver.execute_script("arguments[0].click();", login_button)
 
-# Close window
-input("Press enter to close the browser")
-driver.quit()
+    # Close window
+    input("Press enter to close the browser")
+finally:
+    driver.quit()
