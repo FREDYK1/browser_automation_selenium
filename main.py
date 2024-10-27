94cd070fcdd27e3adee3fa002f7ba4b76d003287
FREDYK1 <fkwekukankam@gmail.com>
2024-10-27 23:34:24 +0000

    Locate, fill and submit form to the webserver

--- main.py
+++ main.py
@@ -11,21 +11,40 @@ chrome_options.add_argument("--disable-search-engine-choice-screen")
 service = Service("chromedriver-win64/chromedriver.exe")
 driver = webdriver.Chrome(options=chrome_options, service=service)
 
-try:
-    # Open the browser and navigate to the URL
-    driver.get("https://demoqa.com/login")
+# Open the browser and navigate to the URL
+driver.get("https://demoqa.com/login")
 
-    # Locate username, password, and login button fields
-    username_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "userName")))
-    password_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "password")))
-    login_button = driver.find_element(By.ID, "login")
+# Locate username, password, and login button fields
+username_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "userName")))
+password_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "password")))
+login_button = driver.find_element(By.ID, "login")
 
-    # Insert username, password, and click login button
-    username_field.send_keys("FREDYKcode1")
-    password_field.send_keys("FREDYK@code1")
-    driver.execute_script("arguments[0].click();", login_button)
+# Insert username, password, and click login button
+username_field.send_keys("FREDYKcode1")
+password_field.send_keys("FREDYK@code1")
+driver.execute_script("arguments[0].click();", login_button)
 
-    # Close window
-    input("Press enter to close the browser")
-finally:
-    driver.quit()
+# Locate the element dropdown and Textbox
+elements  = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/div/div/div/div[1]/div/div/div[1]/span/div')))
+elements.click()
+
+text_box = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "item-0")))
+text_box.click()
+
+#Locate the form fields
+full_name_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "userName")))
+email_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "userEmail")))
+current_address_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "currentAddress")))
+permanent_address_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "permanentAddress")))
+submit_button = driver.find_element(By.ID, "submit")
+
+# Fill the form
+full_name_field.send_keys("Frederick Kankam")
+email_field.send_keys("frederickkankam7@gmail.com")
+current_address_field.send_keys("Mataheko")
+permanent_address_field.send_keys("Tema")
+driver.execute_script("arguments[0].click();", submit_button)
+
+# Close window
+input("Press enter to close the browser")
+driver.quit()
